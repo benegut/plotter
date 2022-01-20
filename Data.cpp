442bdb80df7929683e1040a8598583d67645a2bc
benegut <bgutsche@gmx.de>
2022-01-20 14:17:28 +0100

    fd

--- Data.cpp
+++ Data.cpp
@@ -8,14 +8,14 @@
 #include "Data.h"
 
 
-
-void Data::setPathToFolder(std::string nameOfDataFolder){
-  this->nameOfDataFolder = nameOfDataFolder;
+/*
+void Data::set_Path_To_Folder(std::string nameOfDataFolder){
+  Data::nameOfDataFolder = nameOfDataFolder;
 }
+*/
 
 
-
-int Data::getRowSize(std::string nameOfDataFolder){
+void Data::get_Row_Size(){
 
   std::string filename;
   std::ifstream listfile("list.txt");
@@ -25,13 +25,10 @@ int Data::getRowSize(std::string nameOfDataFolder){
 
   while(std::getline(listfile, filename)){
 
-    std::ifstream datafile("Measurements/" + nameOfDataFolder + "/" + filename);
+    std::ifstream datafile("Measurements/" + Data::nameOfDataFolder + "/" + filename);
     std::string lines;
 
-    //std::cout << "/Measurements/" + nameOfDataFolder + "/" + filename << '\n';                                    
-
     while(std::getline(datafile,lines)){
-      //      std::cout << lines << '\n';
       counterLines++;
     }
 
@@ -43,14 +40,14 @@ int Data::getRowSize(std::string nameOfDataFolder){
 
   int arraySize = counterLines;// - counterFiles*3;
   //  int arraySize = counterLines; //ignore headerlines for now                               
-  return arraySize;
+  Data::rowSize = arraySize;
 }
 
 
 
 
 
-int Data::getColSize(std::string nameOfDataFolder){
+void Data::get_Col_Size(){
 
   int counterTabs = 0.0;
   std::string filename;
@@ -58,7 +55,7 @@ int Data::getColSize(std::string nameOfDataFolder){
   
   std::getline(listfile, filename);
 
-  std::ifstream datafile("Measurements/" + nameOfDataFolder + "/" + filename);
+  std::ifstream datafile("Measurements/" + Data::nameOfDataFolder + "/" + filename);
   std::string line;
   
   std::getline(datafile, line);
@@ -69,56 +66,40 @@ int Data::getColSize(std::string nameOfDataFolder){
   while(std::getline(ss, num, '\t'))
     counterTabs++;
 
-  return counterTabs;
+  Data::colSize = counterTabs;
 }
 
 
 
-void Data::cleanUpMatrix(long double **inputMat, long double **outputMat){
-
-
-
-}
-
-
-
-void Data::getData(){
-}
-
-
-
-
-Data::Data(std::string nameOfDataFolder){
-
-  int rowSize = Data::getRowSize(nameOfDataFolder);
-  int colSize = Data::getColSize(nameOfDataFolder);
-
-  long double** matrix = new long double*[rowSize];
-  for(int i = 0; i < rowSize; ++i){
-    matrix[i] = new long double[colSize];
-  }
-  
-  Data::setPathToFolder(Data::nameOfDataFolder);
-  std::string exec = "#/bin/bash\ncd Measurements/" + nameOfDataFolder + "\nls -1 > ../../list.txt";
+void generate_File_List(){
+  std::string exec = "#/bin/bash\ncd Measurements/" + Data::nameOfDataFolder + "\nls -1 > ../../list.txt";
   system(exec.c_str());
+  
+}
 
+
+
+
+void Data::read_Files(){
+
+  double** matrix = new double*[Data::rowSize];
+  for(int i = 0; i < Data::rowSize; ++i)
+    matrix[i] = new double[Data::colSize];
+  
   std::string filename;
   std::ifstream list("list.txt");
   int i = 0;
   
   while(getline(list, filename)){
-
-    std::ifstream datafile("Measurements/" + nameOfDataFolder + "/" + filename);
+    std::ifstream datafile("Measurements/" + Data::nameOfDataFolder + "/" + filename);
     std::string line;
     
     while(getline(datafile, line, '\n')){
-
       std::stringstream ss(line);
       std::string num;
       int j = 0;
       
-      while(getline(ss, num, '\t')){
-	
+      while(getline(ss, num, '\t')){	
 	try{matrix[i][j] = stold(num);}
 	catch(std::exception& e){i--;break;}
 	j++;
@@ -131,3 +112,29 @@ Data::Data(std::string nameOfDataFolder){
   Data::data = matrix;
 }
 
+
+
+void Data::clean_Up_Matrix(){
+
+  for(int i=0; i!=Data::rowSize; i++)
+    for(int j=0; j!=Data::colSize; j++){
+  }
+}
+
+
+
+void Data::choose_Data(){
+}
+
+
+
+ 
+Data::Data(std::string nameOfDataFolder){
+  //  Data::nameOfDataFolder = nameOfDataFolder;
+  Data::set_Path_To_Folder(nameOfDataFolder);
+  Data::get_Row_Size();
+  Data::get_Col_Size();
+  Data::generate_File_List();
+  Data::read_Files();
+  Data::clean_Up_Matrix();
+}
--- Data.h
+++ Data.h
@@ -9,20 +9,18 @@ class Data{
 
  private:
   std::string nameOfDataFolder;
+  void set_Path_To_Folder(std::string);
+  int rowSize, colSize;
+  void get_Row_Size();
+  void get_Col_Size();
+  void generate_File_List();
+  void read_Files();
+  void clean_Up_Matrix();
   
  public:
-  void setPathToFolder(std::string);
-
-  long double **data;
-  
-  int getRowSize(std::string);
-
-  int getColSize(std::string);
-
-  void cleanUpMatrix(long double **, long double **);
-  
-  void getData();
-
+  double **data;
+  void choose_Data();
+   
   Data(std::string);
   
 };
